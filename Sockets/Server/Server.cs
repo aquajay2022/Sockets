165c29543286435cf9e48ee946e46cea9c648154
Dominykas <97787345+aquajay2022@users.noreply.github.com>
2023-02-23 21:15:12 +0200

    Add files via upload
    Added the Socket solution

---
+++ Sockets/Server/Server.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                byte[] bytes = new byte[1024];
+
+                IPEndPoint ipend = new IPEndPoint(IPAddress.Loopback, 6969);
+
+                Socket sc = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                
+
+                sc.Bind(ipend);
+                Console.Beep();
+                sc.Listen(6969);
+                Socket mysc = sc.Accept();
+                mysc.Receive(bytes);
+                Console.WriteLine("Waiting for a message :P");
+                Console.WriteLine();
+                string hello = Encoding.ASCII.GetString(bytes);
+                Console.WriteLine(hello);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                using (StreamWriter sw = new StreamWriter("C:\\Users\\Dominylas\\Desktop\\SWSR\\ding.txt"))
+                {
+                    sw.WriteLine(ex);
+                    sw.WriteLine(ex.StackTrace);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sockets/Sockets/Client.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+
+namespace Client
+{
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            //Wait a bit for the server
+            Thread.Sleep(5000);
+            
+            //Console.Beep();
+
+            try
+            {
+                string hi = "Helooo Internet";
+                byte[] buffer = Encoding.ASCII.GetBytes(hi);
+
+                IPEndPoint ipend = new IPEndPoint(IPAddress.Loopback, 6969);
+
+                Socket sc = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                sc.Connect(ipend);
+                sc.Send(buffer);
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.ReadKey();
+        }
+    }
+}
